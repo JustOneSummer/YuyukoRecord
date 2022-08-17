76de5225e5d5f89642ecb90775f6f5a73c2638ea
JustOneSummer <myyuyukosama@gmail.com>
2022-08-17 09:37:57 +0800

    下载进度条

--- UpdateFile.Designer.cs
+++ UpdateFile.Designer.cs
@@ -32,6 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(166, 171);
+            this.button1.Location = new System.Drawing.Point(164, 209);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 64);
             this.button1.TabIndex = 1;
@@ -65,11 +66,19 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "点击开始更新";
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 156);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(464, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
             // UpdateFile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(488, 327);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -88,5 +97,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- UpdateFile.cs
+++ UpdateFile.cs
@@ -18,7 +18,10 @@ namespace YuyukoRecord
             label2.Text = "开始下载资源文件...";
             string home = System.Environment.CurrentDirectory + "\\";
             string path = home + "ships.zip";
-            SourcesLoad.GetFile("http://v.wows.shinoaki.com/ships.zip", path);
+            DownLoadFile("http://v.wows.shinoaki.com/ships.zip", "ships.zip", new Action<int, int>((m, v) => {
+                this.progressBar1.Maximum = m;
+                this.progressBar1.Value = v;
+            }));
             label2.Text = "解压资源文件中......";
             string ships = home + "ships";
             FileInfo fileInfo = new FileInfo(path);
@@ -33,6 +36,80 @@ namespace YuyukoRecord
         private void UpdateFile_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        public static bool DownLoadFile(string URL, string Filename, Action<int, int> updateProgress = null)
+        {
+            Stream st = null;
+            Stream so = null;
+            System.Net.HttpWebRequest Myrq = null;
+            System.Net.HttpWebResponse myrp = null;
+            bool flag = false;
+            try
+            {
+                Myrq = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(URL);
+                myrp = (System.Net.HttpWebResponse)Myrq.GetResponse();
+                //从WEB响应得到总字节数
+                long totalBytes = myrp.ContentLength;
+                //更新进度
+                if (updateProgress != null)
+                {
+                    //从总字节数得到进度条的最大值
+                    updateProgress((int)totalBytes, 0);
+                }
+                st = myrp.GetResponseStream();
+                so = new System.IO.FileStream(Filename, System.IO.FileMode.Create);
+                long totalDownloadedByte = 0;
+                byte[] by = new byte[1024];
+                int osize = st.Read(by, 0, (int)by.Length);
+                while (osize > 0)
+                {
+                    //更新文件大小 
+                    totalDownloadedByte = osize + totalDownloadedByte;
+                    Application.DoEvents();
+                    so.Write(by, 0, osize);
+                    //更新进度
+                    if (updateProgress != null)
+                    {
+                        updateProgress((int)totalBytes, (int)totalDownloadedByte);//更新进度条 
+                    }
+                    //读流 
+                    osize = st.Read(by, 0, (int)by.Length);
+                }
+                //更新进度
+                if (updateProgress != null)
+                {
+                    updateProgress((int)totalBytes, (int)totalBytes);
+                }
+                flag = true;
+            }
+            catch (Exception)
+            {
+                flag = false;
+                throw;
+                //return false;
+            }
+            finally
+            {
+                if (Myrq != null)
+                {
+                    Myrq.Abort();//销毁关闭连接
+                }
+                if (myrp != null)
+                {
+                    myrp.Close();//销毁关闭响应
+                }
+                if (so != null)
+                {
+                    so.Close(); //关闭流 
+                }
+                if (st != null)
+                {
+                    st.Close(); //关闭流  
+                }
+            }
+            return flag;
         }
     }
 }
